79405cfdb35e844d3eeb7d1209c98d793bba991c
BxRay95 <bray387@outlook.com>
2023-04-04 16:39:21 -0700

    completed frontend structure criteria

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+   <>
+   <div className='NotFound'>
+    <h1>Oops, Beer is Empty.</h1>
+   </div>
+   </>
+  )
+}
+
+export default NotFound
