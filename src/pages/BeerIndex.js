2afaa27fc1ea3c672c4416b1e7f405d9b69b6e51
Aaron Criner <aaron.criner@me.com>
2023-04-06 14:33:41 -0700

    added functional carousel for index

--- src/pages/BeerIndex.js
+++ src/pages/BeerIndex.js
@@ -8,38 +8,48 @@ import {
   CardText,
   Button,
 } from "reactstrap";
+import { Carousel } from "3d-react-carousal";
 
 const BeerIndex = ({ beers }) => {
+  let slides = beers.map((beers, index) => {
+    return (
+      <Card
+        key={index}
+        style={{
+          width: "18rem",
+        }}
+      >
+        <img alt="Sample" src="https://picsum.photos/300/200" />
+        <CardBody>
+          <CardTitle tag="h5">{beers.name}</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            {beers.brewery}
+          </CardSubtitle>
+          <CardText>{`This ${beers.variety} has a ${beers.primary_flavor} flavor, with hints of ${beers.secondary_flavor}`}</CardText>
+          <Button>
+            <NavLink to={`/beershow/${beers.id}`}>More Info</NavLink>
+          </Button>
+        </CardBody>
+      </Card>
+    );
+  });
+  const callback = function (index) {
+    console.log("callback", index);
+  };
+
   return (
     <>
       <p>BEERS SHOULD DISPLAY BELOW</p>
       <div>
-        {beers.map((beers, index) => {
-          return (
-            <Card
-              key={index}
-              style={{
-                width: "18rem",
-              }}
-            >
-              <img alt="Sample" src="https://picsum.photos/300/200" />
-              <CardBody>
-                <CardTitle tag="h5">{beers.name}</CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  {beers.brewery}
-                </CardSubtitle>
-                <CardText>{`This ${beers.variety} has a ${beers.primary_flavor} flavor, with hints of ${beers.secondary_flavor}`}</CardText>
-                <Button>
-                  <NavLink to={`/beershow/${beers.id}`}>More Info</NavLink>
-                </Button>
-              </CardBody>
-            </Card>
-          );
-        })}
+        <Carousel
+          slides={slides}
+          autoplay={false}
+          interval={1000}
+          onSlideChange={callback}
+        />
       </div>
       <p>BEERS SHOULD DISPLAY ABOVE</p>
     </>
   );
 };
-
 export default BeerIndex;
